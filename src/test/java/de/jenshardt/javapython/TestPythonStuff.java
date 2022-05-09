662114f4f80ca2c11f13bd1c595a5adf0af1671c
Jens Hardt <jenshardt@gmx.net>
2022-05-09 07:30:52 +0200

    using apache

--- src/test/java/de/jenshardt/javapython/TestPythonStuff.java
+++ src/test/java/de/jenshardt/javapython/TestPythonStuff.java
@@ -2,8 +2,10 @@ package de.jenshardt.javapython;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.StringWriter;
 
 import javax.script.ScriptContext;
@@ -11,6 +13,10 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.SimpleScriptContext;
 
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.exec.PumpStreamHandler;
 import org.junit.jupiter.api.Test;
 import org.python.util.PythonInterpreter;
 
@@ -37,6 +43,23 @@ public class TestPythonStuff {
             pyInterp.exec("print('Hello strange world!')");
             assertEquals("Hello strange world!", output.toString().trim());
         }
+    }
+
+    @Test
+    public void UsingApache() throws ExecuteException, IOException {
+        String line = "python " + resolvePythonScriptPath("hello.py");
+        CommandLine cmdLine = CommandLine.parse(line);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream);
+
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setStreamHandler(streamHandler);
+
+        int exitCode = executor.execute(cmdLine);
+        assertEquals(0, exitCode);
+        assertEquals("Hello strange world!", outputStream.toString()
+                .trim());
     }
 
     private String resolvePythonScriptPath(String filename) {
